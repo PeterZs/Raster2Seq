ddb5db0af303dc276e32a11ffebc9fa30c679a5e
hao-pt <tienhaophung@gmail.com>
2026-05-17 18:57:45 -0400

    Clean master history snapshot

---
+++ tools/download_checkpoints.sh
@@ -0,0 +1,10 @@
+# Structured3D
+gdown --fuzzy https://drive.google.com/file/d/1OsqFzusl7qH0H1Od3yY_SziBCjnW6FPo/view?usp=sharing -O checkpoints
+# CubiCasa5K
+gdown --fuzzy https://drive.google.com/file/d/1M32HlYwXw-4Q_uajSCvpbF31UFPzQVHP/view?usp=sharing -O checkpoints
+# Raster2Graph
+gdown --fuzzy https://drive.google.com/file/d/1ZI4hs1iKk2y84S2dpsDHb40wCOPOGrtj/view?usp=sharing -O checkpoints
+
+
+# # Optional: Structured3D-DensityMap
+# gdown --fuzzy https://drive.google.com/file/d/1M32HlYwXw-4Q_uajSCvpbF31UFPzQVHP/view?usp=sharing -O checkpoints
